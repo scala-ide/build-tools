scala-ide/build-tools

commit a7514f134845a98a9ffb3c25a2b5c33546e299c7

Correctly update scala version in MANIFEST templates

build.sbt
@@ -4,18 +4,18 @@ version := "0.4.7-SNAPSHOT"
 
 organization := "org.scala-ide"
 
-scalaVersion := "2.10.3"
+scalaVersion := "2.11.2"
 
 publishMavenStyle := true
 
-publishTo := Some("Typesafe IDE" at "https://private-repo.typesafe.com/typesafe/ide-2.10")
+publishTo := Some("Typesafe IDE" at "https://private-repo.typesafe.com/typesafe/ide-2.11")
 //publishTo := Some(Resolver.file("file",  new File(Path.userHome.absolutePath+"/.m2/repository")))
 
 credentials += Credentials(Path.userHome / ".credentials")
 
 libraryDependencies ++= Seq(
   "org.osgi" % "org.osgi.core" % "4.3.0", 
-  "net.databinder.dispatch" %% "core" % "0.9.1",
+  "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
   "com.typesafe" % "config" % "0.5.2",
   "junit" % "junit" % "4.8.1" % "test",
   "org.slf4j" % "slf4j-simple" % "1.6.4",

src/main/scala/org/scalaide/buildtools/Ecosystem.scala
@@ -27,6 +27,7 @@ object Ecosystem {
 
   /** default location of the manifest file in a bundle project */
   val PluginManifest = "META-INF/MANIFEST.MF"
+  val PluginManifestTemplatesLocation = "resources/META-INF/"
   /** default location of the feature description fise in a feature project */
   val FeatureDescriptor = "feature.xml"
   /** default location of the ecosystem configuration */
@@ -44,19 +45,19 @@ object Ecosystem {
   /** regex to find the given bundle id dependency in a manifest file */
   def idInManifest(id: String) = ("(.*" + id + ")(,?.*)").r
   /** regex to find the given bundle id dependency, with a version number defined, in a manifest file */
-  def idInManifestWithVersion(id: String) = ("(.*)" + id + """;bundle-version="([^"]*)"(,?.*)""").r
+  def idInManifestWithVersion(id: String) = ("(.*" + id + """);bundle-version="([^"]*)"(,?.*)""").r
 
   /** create the partial function finding the dependency line corresponding to the given id, and set the version */
-  def updateVersionInManifest(id: String, version: Version): PartialFunction[String, String] =
-    updateVersionInManifest(idInManifestWithVersion(id), idInManifest(id), version)
+  def updateVersionInManifest(pluginId: String, version: Version): PartialFunction[String, String] =
+    updateVersionInManifest(idInManifestWithVersion(pluginId), idInManifest(pluginId), version)
 
   /** method need to create the partial function */
   private def updateVersionInManifest(idWithVersion: Regex, id: Regex, version: Version): PartialFunction[String, String] = {
-    case line @ idWithVersion(_, currentVersion, _) =>
-      warning("%s has already a version number defined: %s".format(ScalaLibraryId, currentVersion))
-      line
+    case line @ idWithVersion(prefixWithId, currentVersion, suffix) =>
+      warning(s"$prefixWithId has already a version number defined: $currentVersion. Replacing.")
+      s"""$prefixWithId;bundle-version="[$version,$version]"$suffix"""
     case id(prefixWithId, suffix) =>
-      """%s;bundle-version="[%s,%<s]"%s""".format(prefixWithId, version, suffix)
+      s"""$prefixWithId;bundle-version="[$version,$version]"$suffix"""
   }
 
   /** update the given manifest, using the provided version updater */

src/main/scala/org/scalaide/buildtools/UpdateScalaIDEManifests.scala
@@ -105,19 +105,24 @@ class UpdateScalaIDEManifests(scalaVersion: String, m2Repo: File, scalaIDERoot: 
     Source.fromInputStream(manifestInputStream, "UTF-8").getLines().collectFirst { case BundleVersion(v) => v }.get
   }
 
-  /**
-   * Set strict version dependency to Scala library and compiler in the given project.
+  /** Set strict version dependency to Scala library and compiler in the given project.
    */
   private def updateManifest(projectPath: String, scalaLibraryVersion: Version) {
-    val manifestFile = new File(new File(scalaIDERoot, projectPath), PluginManifest)
+    val projectFolder = new File(scalaIDERoot, projectPath)
+    val manifestFile = new File(projectFolder, PluginManifest)
+    val templateFiles = new File(projectFolder, PluginManifestTemplatesLocation).listFiles()
 
-    updateBundleManifest(manifestFile,
-      updateVersionInManifest(ScalaLangLibraryId, scalaLibraryVersion).
-        orElse(updateVersionInManifest(ScalaLangCompilerId, scalaLibraryVersion)).
-        orElse {
-          case line =>
-            line
-        })
+    val files = manifestFile :: templateFiles.to[List]
+
+    files.foreach { file =>
+      updateBundleManifest(file,
+        updateVersionInManifest(ScalaLangLibraryId, scalaLibraryVersion).
+          orElse(updateVersionInManifest(ScalaLangCompilerId, scalaLibraryVersion)).
+          orElse {
+            case line =>
+              line
+          })
+    }
   }
 
 }

src/main/scala/org/scalaide/buildtools/P2Repository.scala
@@ -18,6 +18,8 @@ import scala.collection.mutable.HashMap
 import scala.util.control.Exception.catching
 import java.net.MalformedURLException
 
+import scala.concurrent.ExecutionContext.Implicits.global
+
 /**
  * !!! This object not thread safe !!! It was used in a single threaded system when implemented.
  */
@@ -173,7 +175,7 @@ object P2Repository {
     val tmpFile = File.createTempFile("downloaded-content", ".jar")
     val svc = url(repoUrl) / CompressedContentFile
 
-    val downloadHandle = Http(svc > as.File(tmpFile)(null)).either
+    val downloadHandle = Http(svc > as.File(tmpFile)).either
     //    try {
     // get rid of the exception
     val stringExceptionHandle = for (ex <- downloadHandle.left) yield "Error downloading file " + ex.getMessage()

commit 3fca3da61858604fbcaade7a5cdfd1c6d329003e

Updates tool to scala 2.12 and fixes errors caused by a7514f1

build.sbt
@@ -4,7 +4,7 @@ version := "0.5.3-SNAPSHOT"
 
 organization := "org.scala-ide"
 
-scalaVersion := "2.11.2"
+scalaVersion := "2.12.1"
 
 publishMavenStyle := true
 
@@ -15,7 +15,7 @@ credentials += Credentials(Path.userHome / ".credentials")
 
 libraryDependencies ++= Seq(
   "org.osgi" % "org.osgi.core" % "4.3.0", 
-  "net.databinder.dispatch" %% "dispatch-core" % "0.11.2",
+  "net.databinder.dispatch" %% "dispatch-core" % "0.12.0",
   "com.typesafe" % "config" % "0.5.2",
   "junit" % "junit" % "4.8.1" % "test",
   "org.slf4j" % "slf4j-simple" % "1.6.4",

src/main/scala/org/scalaide/buildtools/Ecosystem.scala
@@ -115,8 +115,10 @@ object Ecosystem {
             Some(EclipseJuno)
           } else if (v.getMajor() == 3 && v.getMinor() < 10) {
             Some(EclipseKepler)
-          } else {
+          } else if (v.getMajor() == 4 && v.getMinor() < 5) {
             Some(EclipseLuna)
+          } else {
+            Some(EclipseNeon)
           }
       }
     }
@@ -131,6 +133,8 @@ object Ecosystem {
   case object EclipseKepler extends EclipseVersion("kepler", "Kepler", "http://download.eclipse.org/releases/kepler/")
 
   case object EclipseLuna extends EclipseVersion("luna", "Luna", "http://download.eclipse.org/releases/luna/")
+
+  case object EclipseNeon extends EclipseVersion("neon", "Neon", "http://download.eclipse.org/releases/neon/")
 
   def findStrictVersion(range: String): Version =
     findStrictVersion(VersionRange(range))

src/main/scala/org/scalaide/buildtools/UpdateScalaIDEManifests.scala
@@ -110,7 +110,9 @@ class UpdateScalaIDEManifests(scalaVersion: String, m2Repo: File, scalaIDERoot: 
   private def updateManifest(projectPath: String, scalaLibraryVersion: Version) {
     val projectFolder = new File(scalaIDERoot, projectPath)
     val manifestFile = new File(projectFolder, PluginManifest)
-    val templateFiles = new File(projectFolder, PluginManifestTemplatesLocation).listFiles().to[List].filterNot(_.getName.endsWith(".original"))
+    val templateFiles = Option(new File(projectFolder, PluginManifestTemplatesLocation).listFiles()).map {
+      _.to[List].filterNot(_.getName.endsWith(".original"))
+    }.getOrElse(Nil)
 
     val files = manifestFile :: templateFiles
 

src/test/scala/org/scalaide/buildtools/TestSupport.scala
@@ -13,7 +13,7 @@ object TestSupport {
   
   cleanTmpFolders()
 
-  final val resourcesFolder = new File(System.getProperty("user.dir"), "src/test/resources")
+  final val resourcesFolder = new File("src/test/resources")
   
   private def cleanTmpFolders() {
     val tmpFolder = new File("target/tmp-test/")
@@ -31,7 +31,8 @@ object TestSupport {
 
     for (folder <- baseFolders) {
       val newFolder = new File(tmpFolder, folder)
-      IOFileUtils.copyDirectory(new File(resourcesFolder, folder), newFolder)
+      val source = new File(resourcesFolder, folder)
+      IOFileUtils.copyDirectory(source, newFolder)
       result.append(newFolder)
     }
 

src/test/scala/org/scalaide/buildtools/UpdateScalaIDEManifestsTest.scala
@@ -22,9 +22,7 @@ class UpdateScalaIDEManifestsTest {
     val sdtDebugManifest = new File(scalaIDERoot, "org.scala-ide.sdt.debug/META-INF/MANIFEST.MF")
 
     checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangCompilerId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
-    checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangLibraryId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
     checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangCompilerId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
-    checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangLibraryId, "2.10.2.v20130530-074427-VFINAL-60d462ef6e")
   }
 
   @Test
@@ -37,10 +35,7 @@ class UpdateScalaIDEManifestsTest {
     val sdtDebugManifest = new File(scalaIDERoot, "org.scala-ide.sdt.debug/META-INF/MANIFEST.MF")
 
     checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangCompilerId, "2.10.3.v20130924-143159-9f62900145")
-    checkRequiredVersionInManifest(sdtCoreManifest, ScalaLangLibraryId, "2.10.3.v20130924-143159-9f62900145")
     checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangCompilerId, "2.10.3.v20130924-143159-9f62900145")
-    checkRequiredVersionInManifest(sdtDebugManifest, ScalaLangLibraryId, "2.10.3.v20130924-143159-9f62900145")
   }
-
 
 }